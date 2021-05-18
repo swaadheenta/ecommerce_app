9431955d6bc6f22d2b2c82191f485a231e8203ff
swaadheenta <gayatriagnani7@gmail.com>
2021-05-18 10:38:03 +0530

    Tabs added in all pages !!

--- lib/Categories/Kitchen/accessories.dart
+++ lib/Categories/Kitchen/accessories.dart
@@ -9,6 +9,17 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+class category {
+  int index;
+  String name;
+  category({this.index, this.name});
+}
+
+String categoryname;
+
+
+
+
 class accessories extends StatefulWidget {
   @override
   _accessoriesState createState() => _accessoriesState();
@@ -31,148 +42,152 @@ class _accessoriesState extends State<accessories> {
   var imgstring;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Enter the details"),
-        backgroundColor: Colors.blue,
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 50.0, left: 10.0, right: 10.0),
-          child: Container(
-            child: Column(
-              children: [
-                Card(
-                  elevation: 7.0,
-                  child: Container(
-                    height: displayHeight(context) * 0.075,
-                    width: displayWidth(context) * 0.95,
-                    child: DropdownButton(
-                      hint: dropdownValue == null
-                          ? Text('Please select a category')
-                          : Padding(
-                              padding: const EdgeInsets.only(left: 15.0),
-                              child: Text(
-                                dropdownValue,
-                                style: TextStyle(color: Colors.blue),
+    return DefaultTabController(
+      length: 2,
+          child: Scaffold(
+        appBar: AppBar(
+          title: Text("Enter the details"),
+          backgroundColor: Colors.blue,
+        ),
+        body: TabBarView(children: [ SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 50.0, left: 10.0, right: 10.0),
+            child: Container(
+              child: Column(
+                children: [
+                  Card(
+                    elevation: 7.0,
+                    child: Container(
+                      height: displayHeight(context) * 0.075,
+                      width: displayWidth(context) * 0.95,
+                      child: DropdownButton(
+                        hint: dropdownValue == null
+                            ? Text('Please select a category')
+                            : Padding(
+                                padding: const EdgeInsets.only(left: 15.0),
+                                child: Text(
+                                  dropdownValue,
+                                  style: TextStyle(color: Colors.blue),
+                                ),
                               ),
-                            ),
-                      isExpanded: true,
-                      iconSize: 30.0,
-                      style: TextStyle(color: Colors.blue),
-                      items: ["Knives, Peelers & Lighters","Kitchen Tools"].map(
-                        (val) {
-                          return DropdownMenuItem<String>(
-                            value: val,
-                            child: Text(val),
+                        isExpanded: true,
+                        iconSize: 30.0,
+                        style: TextStyle(color: Colors.blue),
+                        items: ["Knives, Peelers & Lighters","Kitchen Tools"].map(
+                          (val) {
+                            return DropdownMenuItem<String>(
+                              value: val,
+                              child: Text(val),
+                            );
+                          },
+                        ).toList(),
+                        onChanged: (val) {
+                          setState(
+                            () {
+                              dropdownValue = val;
+                              Categoryname = val;
+                            },
                           );
                         },
-                      ).toList(),
-                      onChanged: (val) {
-                        setState(
-                          () {
-                            dropdownValue = val;
-                            Categoryname = val;
-                          },
-                        );
-                      },
+                      ),
                     ),
                   ),
-                ),
-                TextFormField(
-                  controller: SubCategoryname,
-                  decoration: InputDecoration(
-                    hintText: "Enter the name of subcategory",
+                  TextFormField(
+                    controller: SubCategoryname,
+                    decoration: InputDecoration(
+                      hintText: "Enter the name of subcategory",
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: displayHeight(context) * 0.02,
-                ),
-                TextFormField(
-                  controller: SubCategoryquantity,
-                  decoration: InputDecoration(
-                    hintText: "Enter the quantity of subcateory",
+                  SizedBox(
+                    height: displayHeight(context) * 0.02,
                   ),
-                ),
-                SizedBox(
-                  height: displayHeight(context) * 0.02,
-                ),
-                TextFormField(
-                  controller: SubCategoryoldprice,
-                  decoration: InputDecoration(
-                    hintText: "Enter the old price of subcategory",
+                  TextFormField(
+                    controller: SubCategoryquantity,
+                    decoration: InputDecoration(
+                      hintText: "Enter the quantity of subcateory",
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: displayHeight(context) * 0.05,
-                ),
-                TextFormField(
-                  controller: SubCategorynewprice,
-                  decoration: InputDecoration(
-                    hintText: "Enter the new price of subcategory",
+                  SizedBox(
+                    height: displayHeight(context) * 0.02,
                   ),
-                ),
-                SizedBox(
-                  height: displayHeight(context) * 0.05,
-                ),
-               
-                ButtonTheme(
-                  height: displayHeight(context) * 0.15,
-                  minWidth: displayWidth(context) * 0.32,
-                  child: OutlineButton(
-                    borderSide: BorderSide(color: Colors.grey, width: 2.5),
-                    onPressed: getImage,
-                    child: (_image != null)
-                        ? Image(
-                            image: AssetImage(_image.path),
-                            filterQuality: FilterQuality.high,
-                          )
-                        : Icon(Icons.add, size: 30.0), //
+                  TextFormField(
+                    controller: SubCategoryoldprice,
+                    decoration: InputDecoration(
+                      hintText: "Enter the old price of subcategory",
+                    ),
                   ),
-                ),
-                Opacity(
-                  opacity: 0.0,
-                  child: Divider(),
-                ),
-                Text(
-                  "Double tap on submit button",
-                  style: TextStyle(color: Colors.red),
-                ),
-                SizedBox(
-                  height: displayHeight(context) * 0.05,
-                ),
-                FlatButton(
-                    color: Colors.blue,
-                    onPressed: () {
-                      Map<String, dynamic> data = {
-                        "name": SubCategoryname.text,
-                        "quantity": SubCategoryquantity.text,
-                        "oldprice": SubCategoryoldprice.text,
-                        "newprice":SubCategorynewprice.text,
-                        "imgname": Imgname,
-                        "imgloc": Imgloc,
-                      };
+                  SizedBox(
+                    height: displayHeight(context) * 0.05,
+                  ),
+                  TextFormField(
+                    controller: SubCategorynewprice,
+                    decoration: InputDecoration(
+                      hintText: "Enter the new price of subcategory",
+                    ),
+                  ),
+                  SizedBox(
+                    height: displayHeight(context) * 0.05,
+                  ),
+                 
+                  ButtonTheme(
+                    height: displayHeight(context) * 0.15,
+                    minWidth: displayWidth(context) * 0.32,
+                    child: OutlineButton(
+                      borderSide: BorderSide(color: Colors.grey, width: 2.5),
+                      onPressed: getImage,
+                      child: (_image != null)
+                          ? Image(
+                              image: AssetImage(_image.path),
+                              filterQuality: FilterQuality.high,
+                            )
+                          : Icon(Icons.add, size: 30.0), //
+                    ),
+                  ),
+                  Opacity(
+                    opacity: 0.0,
+                    child: Divider(),
+                  ),
+                  Text(
+                    "Double tap on submit button",
+                    style: TextStyle(color: Colors.red),
+                  ),
+                  SizedBox(
+                    height: displayHeight(context) * 0.05,
+                  ),
+                  FlatButton(
+                      color: Colors.blue,
+                      onPressed: () {
+                        Map<String, dynamic> data = {
+                          "name": SubCategoryname.text,
+                          "quantity": SubCategoryquantity.text,
+                          "oldprice": SubCategoryoldprice.text,
+                          "newprice":SubCategorynewprice.text,
+                          "imgname": Imgname,
+                          "imgloc": Imgloc,
+                        };
 
-                      //FirebaseFirestore.instance.collection("test").add(data);
-                      FirebaseFirestore.instance
-                          .collection(Categoryname)
-                          .doc(SubCategoryname.text)
-                          .set(data);
-                      // .collection(Categoryname.text)
-                      // .doc()
-                      //  .collection("Pulses")
-                      //  .doc(SubCategoryname.text)
-                      //  .set(data);
-                    },
-                    child: Text(
-                      "Submit",
-                      style: TextStyle(color: Colors.white),
-                    )),
-              ],
+                        //FirebaseFirestore.instance.collection("test").add(data);
+                        FirebaseFirestore.instance
+                            .collection(Categoryname)
+                            .doc(SubCategoryname.text)
+                            .set(data);
+                        // .collection(Categoryname.text)
+                        // .doc()
+                        //  .collection("Pulses")
+                        //  .doc(SubCategoryname.text)
+                        //  .set(data);
+                      },
+                      child: Text(
+                        "Submit",
+                        style: TextStyle(color: Colors.white),
+                      )),
+                ],
+              ),
             ),
           ),
         ),
-      ),
+     ],)
+        ),
     );
   }
 
